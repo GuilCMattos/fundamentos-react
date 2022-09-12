e8483d2e505466122787e07a2ba9af33dec050c9
Guilherme <mttsguil@gmail.com>
2022-09-12 11:18:43 -0300

    add: css in react

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
+import './index.css'
 import ReactDOM from 'react-dom'
+import React from 'react'
 
-const el = document.getElementById('root')
+const el = document.getElementById('root');
+const tag = <strong>Olá React!</strong>
 
-ReactDOM.render('Olá react', el)
+ReactDOM.render(
+<div>
+    {tag}
+    </div>, 
+el)
