bdcb3d55f6d54b0817b93009b9f10f04da3b523f
Guilherme <mttsguil@gmail.com>
2022-09-12 12:13:05 -0300

    add: react fragment

--- src/index.js
+++ src/index.js
@@ -4,17 +4,19 @@ import React from 'react'
 
 import First from './components/basics/First';
 import ComParametro from './components/basics/ComParametro';
+import Fragment from './components/basics/Fragment';
 
 const el = document.getElementById('root');
 
 
 ReactDOM.render(
-<div>
+<>
     <First/>
     <ComParametro title="Situação Aluno" aluno="Pedro Silva" nota={6} />
+    <Fragment />
 
     
 
 
-    </div>, 
+    </>, 
 el)
