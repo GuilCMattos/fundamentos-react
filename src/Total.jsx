b9828cec8c00d77a604d28a92340376c29a5c348
Guilherme <mttsguil@gmail.com>
2022-09-13 08:09:05 -0300

    add: cards component children

--- src/Total.jsx
+++ src/Total.jsx
@@ -5,15 +5,30 @@ import First from "./components/basics/First";
 import ComParametro from "./components/basics/ComParametro";
 import Fragment from "./components/basics/Fragment";
 import RadomNumber from "./components/basics/RadomNumber";
+import Card from "./components/layout/Card";
 
 
 
 export default props => 
         <div id="app">      
-                    <h1>Fundamentos React</h1>    
-                    <Fragment />
-                    <ComParametro title="Situação Aluno" aluno="Pedro Silva" nota={6} />
-                    <First/>
-                    <RadomNumber max={100} min={10} />
+
+                    <h1>Fundamentos React</h1>   
+                    <Card title="#04 - Desafio Aleatório"> 
+                        <RadomNumber max={60} min={1} />
+                    </Card> 
+
+                    <Card title="#03 - Fragmento"> 
+                         <Fragment />
+                    </Card> 
+
+                    <Card title="#02 - Aluno"> 
+                          <ComParametro title="Situação Aluno" aluno="Pedro Silva" nota={6} />
+                    </Card> 
+
+                    <Card title="#01 - Primeiro"> 
+                        <First/>
+                    </Card> 
+                    
+                    
 
             </div>
